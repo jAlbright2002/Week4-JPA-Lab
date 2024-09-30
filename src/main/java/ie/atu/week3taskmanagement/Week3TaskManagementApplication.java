eaa1ed1fc4c08a3713f17ad9c12fb6f97fa5349c
James A <G00379271@atu.ie>
2024-09-30 16:27:36 +0100

    Create project

---
+++ src/main/java/ie/atu/week3taskmanagement/Week3TaskManagementApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week3taskmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week3TaskManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week3TaskManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week3taskmanagement/Week3TaskManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week3taskmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week3TaskManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
