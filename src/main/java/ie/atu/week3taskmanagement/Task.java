f8b2ddf89cc578cc21481a811e85893e77aa52e0
James A <G00379271@atu.ie>
2024-09-30 16:37:49 +0100

    Create Task POJO

---
+++ src/main/java/ie/atu/week3taskmanagement/Task.java
@@ -0,0 +1,14 @@
+package ie.atu.week3taskmanagement;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Task {
+    private int id;
+    private String task;
+    private String status;
+}
