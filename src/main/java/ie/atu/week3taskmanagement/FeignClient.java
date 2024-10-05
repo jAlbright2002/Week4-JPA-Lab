4449678da4722386e7d670d022ff738e5f44d845
james <G00379271@atu.ie>
2024-10-05 18:07:42 +0100

    adjust feign url missed specifying the images

--- src/main/java/ie/atu/week3taskmanagement/FeignClient.java
+++ src/main/java/ie/atu/week3taskmanagement/FeignClient.java
@@ -2,7 +2,7 @@ package ie.atu.week3taskmanagement;
 
 import org.springframework.web.bind.annotation.GetMapping;
 
-@org.springframework.cloud.openfeign.FeignClient(name = "TaskNotification", url = "http://localhost:8081")
+@org.springframework.cloud.openfeign.FeignClient(name = "TaskNotification", url = "http://task-notification:8081")
 public interface FeignClient {
 
     @GetMapping("/updated")
