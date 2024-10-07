6f15a619a30649af151b6bedce6fb36bf53b9397
James A <G00379271@atu.ie>
2024-10-07 17:03:25 +0100

    Remove FeignClient
    Move all service logic to JPA.
    
    Controller only routes endpoints

--- src/main/java/ie/atu/week3taskmanagement/TaskManagementService.java
+++ src/main/java/ie/atu/week3taskmanagement/TaskManagementService.java
@@ -5,23 +5,18 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TaskManagementService {
 
-    private final FeignClient client;
-
     private TaskRepository taskRepo;
 
     @Autowired
-    public TaskManagementService(FeignClient client, TaskRepository taskRepo) {
+    public TaskManagementService(TaskRepository taskRepo) {
         this.taskRepo = taskRepo;
-        this.client = client;
     }
 
-    private List<Task> taskList = new ArrayList<>();
-
-    String notification;
 
     public List<Task> getTasks() {
         return taskRepo.findAll();
@@ -29,25 +24,17 @@ public class TaskManagementService {
 
     public void addTask(Task task) {
         taskRepo.save(task);
-//        notification = client.addedTask();
-//        System.out.println(notification);
     }
 
-    public void updateTask(Task task, int id) {
-        for (Task task1 : taskList) {
-            if (task1.getId() == id) {
-                task1.setTask(task.getTask());
-                task1.setStatus(task.getStatus());
-            }
-        }
-        notification = client.updatedTask();
-        System.out.println(notification);
+    public void updateTask(Long id, Task task) {
+        Task updatedTask = taskRepo.getReferenceById(id);
+        updatedTask.setTask(task.getTask());
+        updatedTask.setStatus(task.getStatus());
+        taskRepo.save(updatedTask);
     }
 
-    public void removeTask(int id) {
-        taskList.removeIf(task -> task.getId() == id);
-        notification = client.removedTask();
-        System.out.println(notification);
+    public void removeTask(Long id) {
+        taskRepo.deleteById(id);
     }
 
 }
