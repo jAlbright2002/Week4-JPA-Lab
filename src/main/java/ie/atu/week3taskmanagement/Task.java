7334bcfcdd126c36be957cf694feea034a6ea370
James A <G00379271@atu.ie>
2024-10-07 15:28:57 +0100

    add new application properties file
    fix pom, incorrect dependencies

--- src/main/java/ie/atu/week3taskmanagement/Task.java
+++ src/main/java/ie/atu/week3taskmanagement/Task.java
@@ -1,5 +1,9 @@
 package ie.atu.week3taskmanagement;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
@@ -9,11 +13,16 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Task {
-    @PositiveOrZero(message = "ID must be 0 or more")
-    private int id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     @NotEmpty(message = "task must not be empty")
     private String task;
+
     @NotEmpty(message = "status must not be empty")
     private String status;
 }
